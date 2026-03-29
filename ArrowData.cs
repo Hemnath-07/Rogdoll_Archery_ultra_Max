5cba428223e288fbbcb282a8050097ba8cf19e87
Hemnath <hemu.suku2005@gmail.com>
2026-03-29 19:52:16 +0530

    Update ArrowData.cs

--- ArrowData.cs
+++ ArrowData.cs
@@ -3,16 +3,21 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "ArrowData", menuName = "Game/Arrow Data")]
 public class ArrowData : ScriptableObject
 {
-    public string arrowName;
+    public ArrowType arrowType;
 
-    [Header("Stats")]
-    public float damage = 25f;
+    [Header("Base Stats")]
+    public float damage = 20f;
     public float speed = 20f;
     public float lifeTime = 5f;
 
     [Header("Physics")]
     public bool affectedByGravity = true;
+    public float mass = 1f;
+
+    [Header("Special")]
+    public bool canHeadshot = true;
+    public bool destroyOnHit = true;
 
     [Header("Effects")]
-    public ArrowEffect[] effects;
+    public bool useEffect;
 }
