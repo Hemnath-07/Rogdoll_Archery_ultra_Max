3fff0a7f9d3e598ea4f49a5696541851666acc23
Hemnath <hemu.suku2005@gmail.com>
2026-03-29 19:26:40 +0530

    Create ArrowType.cs

---
+++ ArrowType.cs
@@ -0,0 +1,28 @@
+public enum ArrowType
+{
+    Normal,
+
+    // Elemental
+    Fire,
+    Ice,
+    Lightning,
+    Poison,
+
+    // Impact
+    Explosive,
+    Knockback,
+    Piercing,
+    Shockwave,
+
+    // Tactical
+    Teleport,
+    Trap,
+    Rope,
+    Gravity,
+
+    // Special
+    TimeSlow,
+    MultiShot,
+    Homing,
+    Clone
+}
